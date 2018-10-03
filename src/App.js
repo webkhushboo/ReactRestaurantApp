7fa71793fed633cdd8869fd9ddb9170c4d3df883
Khushboo Agrawal <khagrawa@adobe.com>
2018-10-03 15:32:51 +0530

    Added first component in react app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -11,6 +20,7 @@ class App extends Component {
          <NavbarBrand href="/">Ristorenate Con Fusion</NavbarBrand>
          </div>
        </Navbar>
+       <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
