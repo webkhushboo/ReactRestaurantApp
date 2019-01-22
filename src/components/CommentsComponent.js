e8b5222130bb5432e2c4dc0048c03cb81b65c4b5
Khushboo Agrawal <web.khushboo@gmail.com>
2019-01-22 15:44:53 +0530

    Assignment submit comments in react redux form done

--- src/components/CommentsComponent.js
+++ src/components/CommentsComponent.js
@@ -1,4 +1,162 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label,
+  Col,
+  Row
+} from "reactstrap";
+
+import { Control, LocalForm, Errors } from "react-redux-form";
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmitComments = this.handleSubmitComments.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleSubmitComments(values) {
+    console.log("Current state is :" + JSON.stringify(values));
+    alert(console.log("Current state is :" + JSON.stringify(values)));
+  }
+
+  render() {
+    if (this.props.comments.length > 0) {
+      const comments = this.props.comments.map(comment => {
+        return (
+          <div className="col-12" key={comment.id}>
+            <RenderComments comment={comment} />
+          </div>
+        );
+      });
+      return (
+        <div className="row">
+          {comments}
+          <Button className="ml-3" outline onClick={this.toggleModal}>
+            <span className="fa fa-pencil fa-lg" /> Submit Comment
+          </Button>
+          <div className="col-12">
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+              <ModalHeader toggle={this.toggleModal}>
+                Submit Comment
+              </ModalHeader>
+              <ModalBody>
+                <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                  <Row className="form-group">
+                    <Label htmlFor="rating" md={12}>
+                      Rating
+                    </Label>
+                    <Col md={12}>
+                      <Control.text
+                        model=".rating"
+                        id="rating"
+                        name="rating"
+                        className="form-control"
+                        placeholder="Rating"
+                        type="number"
+                        validators={{
+                          required
+                        }}
+                      />
+                      <Errors
+                        className="text-danger"
+                        model=".rating"
+                        show="touched"
+                        messages={{
+                          required: "Required "
+                        }}
+                      />
+                    </Col>
+                  </Row>
+                  <Row className="form-group">
+                    <Label md={12} htmlFor="name">
+                      Last Name
+                    </Label>
+                    <Col md={12}>
+                      <Control.text
+                        model=".name"
+                        id="name"
+                        name="name"
+                        placeholder="Last Name"
+                        className="form-control"
+                        validators={{
+                          required,
+                          minLength: minLength(3),
+                          maxLength: maxLength(15)
+                        }}
+                      />
+                      <Errors
+                        className="text-danger"
+                        model=".name"
+                        show="touched"
+                        messages={{
+                          required: "Required ",
+                          minLength: "Must be greater than 2 characaters",
+                          maxLength: "Must be 15 characters or less"
+                        }}
+                      />
+                    </Col>
+                  </Row>
+                  <Row className="form-group">
+                    <Label htmlFor="comments" md={12}>
+                      Comments
+                    </Label>
+                    <Col md={12}>
+                      <Control.textarea
+                        model=".comments"
+                        id="comments"
+                        name="comments"
+                        placeholder="Enter comments"
+                        className="form-control"
+                        rows="5"
+                        validators={{
+                          required
+                        }}
+                      />
+                      <Errors
+                        className="text-danger"
+                        model=".comments"
+                        show="touched"
+                        messages={{
+                          required: "Required "
+                        }}
+                      />
+                    </Col>
+                  </Row>
+                  <Row className="form-group">
+                    <Col md={{ size: 10, offset: 2 }}>
+                      <Button type="submit" color="primary">
+                        Submit
+                      </Button>
+                    </Col>
+                  </Row>
+                </LocalForm>
+              </ModalBody>
+            </Modal>
+          </div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  }
+}
 
 function RenderComments({ comment }) {
   return (
@@ -18,19 +176,4 @@ function RenderComments({ comment }) {
   );
 }
 
-const Comments = props => {
-  if (props.comments.length > 0) {
-    const comments = props.comments.map(comment => {
-      return (
-        <div className="col-12" key={comment.id}>
-          <RenderComments comment={comment} />
-        </div>
-      );
-    });
-    return <div className="row">{comments}</div>;
-  } else {
-    return <div />;
-  }
-};
-
-export default Comments;
+export default Comment;
