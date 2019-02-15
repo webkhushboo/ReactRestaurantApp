8df6f681295b40c2d83428cc2c595095a21df6f9
Khushboo Agrawal <web.khushboo@gmail.com>
2019-02-15 13:06:31 +0530

    Redux thunk implementation

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -10,6 +10,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import Comments from "../components/CommentsComponent";
+import { Loading } from "../components/LoadingComponent";
 
 function RenderDish({ dish }) {
   if (dish != null) {
@@ -28,30 +29,49 @@ function RenderDish({ dish }) {
 }
 
 const DishDetail = props => {
-  return (
-    <div className="container">
-      <div className="col-12">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/menu">Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>
-            <Link to={`/menu/${props.dish.id}`}>{props.dish.name}</Link>
-          </BreadcrumbItem>
-        </Breadcrumb>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <h1> {props.dish.name}</h1>
-          <RenderDish dish={props.dish} />
-        </div>
-        <div className="col-6">
-          <h1> Comments</h1>
-          <Comments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
         </div>
       </div>
-    </div>
-  );
+    )
+  }
+  else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    )
+  }
+  else
+    return (
+      <div className="container">
+        <div className="col-12">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>
+              <Link to={`/menu/${props.dish.id}`}>{props.dish.name}</Link>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <h1> {props.dish.name}</h1>
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-6">
+            <h1> Comments</h1>
+            <Comments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
+          </div>
+        </div>
+      </div>
+    );
 };
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,24 +8,46 @@ import {
   BreadcrumbItem
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Loading } from '../components/LoadingComponent';
 
-function RenderMenuItem({ dish }) {
-  return (
-    <Card>
-      <Link to={`/menu/${dish.id}`}>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
-        <CardImgOverlay>
-          <CardTitle>{dish.name}</CardTitle>
-        </CardImgOverlay>
-      </Link>
-    </Card>
-  );
+function RenderMenuItem({ dish, isLoading, errMess }) {
+  if (isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    )
+  }
+  else if (errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{errMess}</h4>
+        </div>
+      </div>
+    )
+  }
+  else
+
+    return (
+      <Card>
+        <Link to={`/menu/${dish.id}`}>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardImgOverlay>
+            <CardTitle>{dish.name}</CardTitle>
+          </CardImgOverlay>
+        </Link>
+      </Card>
+    );
 }
 const Menu = props => {
-  const menu = props.dishes.map(dish => {
+  const menu = props.dishes.dishes.map(dish => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
-        <RenderMenuItem dish={dish} />
+        <RenderMenuItem dish={dish} isLoading={dish.isLoading}
+          errMess={dish.errMess} />
       </div>
     );
   });
